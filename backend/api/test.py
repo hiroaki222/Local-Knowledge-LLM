f104c9a3b43c8452afa89ee2d62fc7ea7a7e9d34
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-07-11 07:45:54 +0000

    fix: 0.0.0.0に固定して`python test.py`で実行できるようにした(フロントエンドからアクセスできるように)

--- backend/api/test.py
+++ backend/api/test.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -11,3 +11,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
