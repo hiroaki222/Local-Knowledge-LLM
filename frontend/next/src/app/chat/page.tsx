4bc84a574f0b46dbf7e99a9fe20a722397def303
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-07-12 02:35:40 +0000

    fix: フッターのバグ修正

--- frontend/next/src/app/chat/page.tsx
+++ frontend/next/src/app/chat/page.tsx
@@ -6,27 +6,22 @@ import { Input } from "@/components/ui/input"
 
 export default function Page() {
     return <>
-
-        <div className="flex h-screen bg-sky-100 ">
+        <div className="flex h-screen bg-sky-100">
             <aside className="w-80 border-r dark:border-zinc-700">
             </aside>
-            <section className="flex flex-col w-full">
+            <section className="flex flex-col w-full relative">
                 <Header />
                 <div className="flex w-full absolute bottom-0 justify-center items-center h-16 bg-sky-300">
                     {/* <Footer/> */}
                     {/* <div className="flex w-full justify-center items-center h-16 bg-sky-300 "> */}
-                        <div className="flex w-full items-center space-x-2">
+                        {/* <div className="flex w-full items-center space-x-2"> */}
+                        <div className="flex w-full items-center">
                             <Input type="query" placeholder="query" />
                             <Button type="submit">送信</Button>
                         </div>
                     {/* </div> */}
                 </div>
             </section>
-
-
         </div>
-
     </>
-
-
 }
