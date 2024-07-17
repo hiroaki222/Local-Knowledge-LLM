efd826c06fe6a0990a630ea9a3b6152c877df76e
Shion0404 <twingang8016@gmail.com>
2024-07-17 04:46:38 +0000

    feat: caht機能

--- frontend/next/src/app/chat/page.tsx
+++ frontend/next/src/app/chat/page.tsx
@@ -3,14 +3,24 @@ import Header from "@/components/ui/header"
 import Footer from "@/components/ui/footer"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import React, { useState } from "react"
 
 
-const ChatForm = () =>{
-    
-}
 
 
 export default function Page() {
+    const [prompt, setPrompt] = useState('N/A')
+    const [result, setResult] = useState('N/A')
+
+    async function fetchAPI(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        try {
+            const data = await (await fetch(`/api/DqhSF91M_Eyl1rWXIrJI7gtFnjB67TSMJV2nf9-ZuNVHNDg?prompt=${encodeURIComponent(prompt)}`)).json()
+            setResult(data.response)
+        } catch (error) {
+            setResult('error')
+        }
+    }
     return <>
         <div className="flex h-screen bg-sky-50">
             <aside className="w-80 border-r dark:border-zinc-700 bg-sky-100">
@@ -20,9 +30,18 @@ export default function Page() {
             </aside>
             <section className="flex flex-col w-full relative">
                 <Header />
-                <div className="flex w-full absolute bottom-0">
-                    <Footer />
-                </div>
+                <div className="text">{result}</div>
+                <form onSubmit={fetchAPI}>
+                    <div className="flex w-full absolute bottom-0">
+                        <footer className="flex w-full justify-center items-center h-16 bg-sky-300 ">
+                            <div className="flex w-full items-center spave-x-2 ml-1 mr-1">
+                                <Input type="prompt" name="prompt" placeholder="prompt" className="border" onChange={(e) => setPrompt(e.target.value)} />
+                                <Button type="submit">送信</Button>
+                            </div>
+                        </footer>
+                    </div>
+                </form>
+
             </section>
         </div>
     </>
--- frontend/next/src/components/ui/header.tsx
+++ frontend/next/src/components/ui/header.tsx
@@ -1,7 +1,7 @@
 
 export default function Headers (){
 return<header className="flex w-full justify-center items-center h-16 bg-sky-300">
-        <p className="text-xl text-white font-bold font-sans">橿原チャット</p>
+        <p className="text-xl text-white font-bold font-sans">チャット</p>
        
 </header>
 }
