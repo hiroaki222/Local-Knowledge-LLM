9b9b67f15b29f805ca64ad98140b9ac16a82e74f
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-07-30 16:58:05 +0000

    refactor: 掃除

--- frontend/src/app/ababa/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { ldapSignUp } from "@/lib/actions/ldap-signup"
-
-export default function Page() {
-  return <button onClick={() => ldapSignUp("aaaaa", "bbbbb")}>abba</button>
-}
