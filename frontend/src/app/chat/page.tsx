2a090b762c566172cbc69c51402222807df6b752
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-07-31 08:43:07 +0000

    feat: 削除実装

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,21 +1,20 @@
-import Alert from '@/components/Alert'
 import ChatView from '@/components/ChatView'
 import Header from '@/components/Header'
 import PromptForm from '@/components/PromptForm'
 import ThreadList from '@/components/ThreadList'
 
 export default function Chat() {
+  const uid = '56b7c890-d12e-5678-f901-4567g8901h23'
   return (
     <div className="flex h-screen w-full flex-col bg-background">
       <Header />
       <div className="flex flex-1 overflow-hidden">
-        <ThreadList />
+        <ThreadList uid={uid} />
         <div className="flex flex-1 flex-col">
           <ChatView />
           <PromptForm />
         </div>
       </div>
-      <Alert />
     </div>
   )
 }
--- frontend/src/components/ThreadList.tsx
+++ frontend/src/components/ThreadList.tsx
@@ -1,8 +1,101 @@
 'use client'
-import { Button } from '@/components/ui/button'
 
-export default function ThreadList() {
-  const thread = ''
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+import { Trash } from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
+
+export default function ThreadList({ uid }: { uid: string }) {
+  const [threads, setThreads] = useState([])
+  const [isAlertOpen, setIsAlertOpen] = useState(false)
+  const [threadDelete, setThreadDelete] = useState({ threadId: null, title: null })
+  const hasLoadedBefore = useRef(true)
+
+  async function fetchThreads(uid: string) {
+    const threadList = []
+    try {
+      const response = await (await fetch(`/api/thread?uid=${uid}`)).json()
+      const titles = response.threadsInfo
+      for (let i = 0; i < titles.length; i++) {
+        threadList.push(
+          <div
+            className="flex w-full cursor-pointer items-center justify-between rounded-md bg-muted/50 p-2 transition-colors hover:bg-muted"
+            key={i}
+            onClick={() => handleClick(titles[i].threadId)}
+          >
+            <div className="grow text-left">
+              <div className="truncate">
+                <div className="text-sm text-foreground">{titles[i].title}</div>
+              </div>
+            </div>
+            <button
+              className="p-2"
+              onClick={(e) => {
+                e.stopPropagation()
+                handleDelete({ threadId: titles[i].threadId, title: titles[i].title })
+              }}
+            >
+              <Trash className="size-4" />
+            </button>
+          </div>,
+        )
+      }
+
+      setThreads(threadList)
+    } catch (error) {
+      console.error(error)
+      setThreads(<></>)
+    }
+  }
+
+  useEffect(() => {
+    if (hasLoadedBefore.current) {
+      fetchThreads(uid)
+      hasLoadedBefore.current = false
+    }
+  })
+
+  function handleClick(threadId: string) {
+    console.log(threadId)
+  }
+
+  function handleDelete({ threadId, title }: { threadId: string; title: string }) {
+    setThreadDelete({ threadId: threadId, title: title })
+    setIsAlertOpen(true)
+  }
+
+  function confirmDelete() {
+    if (threadDelete) {
+      ;(async () => {
+        try {
+          const response = await fetch(`/api/thread?threadId=${threadDelete.threadId}`, {
+            method: 'DELETE',
+          })
+          await fetchThreads(uid)
+        } catch (error) {
+          console.error(error)
+        }
+      })()
+
+      setThreadDelete({ threadId: null, title: null })
+    }
+    setIsAlertOpen(false)
+  }
+
+  function cancelDelete() {
+    setThreadDelete({ threadId: null, title: null })
+    setIsAlertOpen(false)
+  }
+
   function handleNewThread(a, b) {}
   return (
     <div className="hidden w-60 border-r bg-background p-4 sm:block">
@@ -19,7 +112,19 @@ export default function ThreadList() {
           <span className="sr-only">新しいスレッド</span>
         </Button>
       </div>
-      <div className="space-y-2 overflow-auto">{thread}</div>
+      <div className="space-y-2 overflow-auto">{threads}</div>
+      <AlertDialog onOpenChange={setIsAlertOpen} open={isAlertOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>「{threadDelete.title}」を本当に削除しますか？</AlertDialogTitle>
+            <AlertDialogDescription>この操作は取り消せません。データが完全に削除されます。</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel onClick={cancelDelete}>キャンセル</AlertDialogCancel>
+            <AlertDialogAction onClick={confirmDelete}>削除</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   )
 }
