7611d79e20aa63d670dfd4b9af5d9b8a51ddd876
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-08-02 06:09:05 +0000

    fix: スレッドが無いときの処理

--- frontend/src/components/ChatView.tsx
+++ frontend/src/components/ChatView.tsx
@@ -3,12 +3,17 @@
 import type { Thread } from '@/lib/types/user'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { useRouter } from 'next/navigation'
 import { useEffect, useRef } from 'react'
 
 export default function ChatView({ thread }: { thread: Thread }) {
   const ref = useRef<HTMLDivElement>(null)
+  const router = useRouter()
 
   useEffect(() => {
+    if (thread.success == false){
+      router.replace('/chat')
+    }
     ref.current?.scrollIntoView({ behavior: 'smooth' })
   }, [thread])
 
@@ -17,7 +22,7 @@ export default function ChatView({ thread }: { thread: Thread }) {
       <div className="flex-1 overflow-hidden px-4 py-6 sm:px-6">
         <div className="h-full pr-4">
           <div className="grid gap-4">
-            {thread.chatLog.map((v, i) =>
+            {thread?.chatLog?.map((v, i) =>
               v.role == 'user' ? (
                 <div className="flex items-start justify-end gap-4" key={`user-${i}`}>
                   <div className="grid gap-1">
