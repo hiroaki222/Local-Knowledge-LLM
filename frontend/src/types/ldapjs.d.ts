0acd41fa8bca48edbb8f71f4cd0be45655be91ea
shallow90125 <shallow90125@gmail.com>
2024-08-04 21:14:23 +0000

    refactor: remove unused files

--- frontend/src/types/ldapjs.d.ts
+++
@@ -1,9 +0,0 @@
-// declare module 'ldapjs' {
-//   export interface Client {
-//     bind(dn: string, password: string, callback: (error: Error | null) => void): void
-//     unbind(): void
-//     add(dn: string, entry: object, callback: (error: Error | null) => void): void
-//   }
-
-//   export function createClient(options: { url: string }): Client
-// }
